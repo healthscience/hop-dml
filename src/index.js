3b1004501cdc10bbf419c227a461a207cb72a06c
aboynejames <james@aboynejames.co.uk>
2023-08-26 23:18:50 +0100

    decentralise machine learning protocol - peer to peer

---
+++ src/index.js
@@ -0,0 +1,31 @@
+'use strict'
+/**
+*  
+*
+*
+* @class HopDml
+* @package    dml
+* @copyright  Copyright (c) 2023 James Littlejohn
+* @license    http://www.gnu.org/licenses/old-licenses/gpl-3.0.html
+* @version    $Id$
+*/
+import EventEmitter from 'events'
+
+class HopDml extends EventEmitter {
+
+  constructor() {
+    super()
+  }
+
+  /**
+  * produce proof of work merkle
+  * @method powEvidence
+  *
+  */
+  powEvidence = function () {
+    
+  }
+
+}
+
+export default HopDml
---
+++ test/pow-test.js
@@ -0,0 +1,9 @@
+import assert from 'assert'
+import DmlLive from '../src/index.js'
+
+describe('challenge proof of work', function () {
+  it('test properly form proof', function () {
+    let DML = new DmlLive()
+    // assert.equal(bbAI.hello, 'bb-AI--{{hello}}')
+  })
+})
